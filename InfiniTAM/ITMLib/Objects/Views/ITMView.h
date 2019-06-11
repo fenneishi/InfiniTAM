9c464925dd5b881cfe94f3490e278c06092a6d91
龙 齐 <longqi@longdeMacBook-Pro.local>
2019-06-11 19:40:26 +0800

    add *

--- InfiniTAM/ITMLib/Objects/Views/ITMView.h
+++ InfiniTAM/ITMLib/Objects/Views/ITMView.h
@@ -10,42 +10,44 @@ namespace ITMLib
 	/** \brief
 	    Represents a single "view", i.e. RGB and depth images along
 	    with all intrinsic and relative calibration information
+	    代表单个"观察"，即"RGB+depth+相机内参+深度图和彩色图对齐内参"组成一个"view"
+	    我感觉就是整个当前帧了
 	*/
 	class ITMView
 	{
 	public:
-		/// Intrinsic calibration information for the view.
+		/// Intrinsic calibration information for the view. 内参
 		const ITMRGBDCalib calib;
 
-		/// RGB colour image for the current frame.
+		/// RGB colour image for the current frame.  彩色图
 		ITMUChar4Image *rgb; 
 
-		/// RGB colour image for the previous frame.
+		/// RGB colour image for the previous frame. 前一帧的彩色图
 		ITMUChar4Image *rgb_prev; 
 
-		/// Float valued depth image, if available according to @ref inputImageType.
+		/// Float valued depth image, if available according to @ref inputImageType.浮点类型的深度图，根据inputImageType来确定是否可用
 		ITMFloatImage *depth;
 
-		/// surface normal of depth image
-		// allocated when needed
+		/// surface normal of depth image 深度图的表面法向量图
+		// allocated when needed  需要时再分配
 		ITMFloat4Image *depthNormal;
 
-		/// uncertainty (std) in each pixel of depth value based on sensor noise model
-		/// allocated when needed
+		/// uncertainty (std) in each pixel of depth value based on sensor noise model 每个像素的深度值不确定度（根据相机的noise model确定）
+		/// allocated when needed 需要时再分配
 		ITMFloatImage *depthUncertainty;
 
-		// confidence based on distance from center
+		// confidence based on distance from center  depth置信度（根据距离光心得距离）
 		ITMFloatImage *depthConfidence;
 
 		ITMView(const ITMRGBDCalib& calibration, Vector2i imgSize_rgb, Vector2i imgSize_d, bool useGPU)
 		: calib(calibration)
 		{
 			this->rgb = new ITMUChar4Image(imgSize_rgb, true, useGPU);
-			this->rgb_prev = NULL;
+			this->rgb_prev = NULL; // 这里默认是null，要注意。
 			this->depth = new ITMFloatImage(imgSize_d, true, useGPU);
 			this->depthNormal = NULL;
 			this->depthUncertainty = NULL;
-			this->depthConfidence = new ITMFloatImage(imgSize_d, true, useGPU);
+			this->depthConfidence = new ITMFloatImage(imgSize_d, true, useGPU); // 按理说，这里不应该是距离越远，置信度越低吗？怎么直接用的深度图
 		}
 
 		virtual ~ITMView(void)
--- InfiniTAM/ITMLib/Trackers/Interface/ITMTracker.h
+++ InfiniTAM/ITMLib/Trackers/Interface/ITMTracker.h
@@ -10,6 +10,7 @@ namespace ITMLib
 	/** \brief
 	    Basic interface to any sort of trackers that will align an
 	    incoming view with an existing scene.
+	    基础接口：用来对齐新帧和已经场景
 	*/
 	class ITMTracker
 	{
@@ -17,17 +18,22 @@ namespace ITMLib
 		/** Gets whether the tracker can keep tracking or not.
 		    Can be used to signal e.g. the end of a sequence
 		    of file-based poses, or the failure of an IMU.
+		    获取tracker是否能够继续追踪
+		    可以用来给"结束帧，位姿计算失败，因为IMU的失败"做signal。
 		*/
 		virtual bool CanKeepTracking() const { return true; }
 
 		/** Localize a View in the given scene. The result is
 		    currently stored as an attribute in trackingState.
+		    定位给定场景的位置。返回值存储在trackingState的属性中
 		*/
 		virtual void TrackCamera(ITMTrackingState *trackingState, const ITMView *view) = 0;
 
 		/** Updates the initial pose of the depth camera in the scene.
 		    This can be used to make the scene up vector correspond
 		    to the real world's up direction.
+		    更新场景中深度相机的初始位姿。
+		    可以用来一致化场景的up vector与真实世界的up direction.
 		*/
 		virtual void UpdateInitialPose(ITMTrackingState *trackingState) {}
 
@@ -38,3 +44,10 @@ namespace ITMLib
 		virtual ~ITMTracker(void) {}
 	};
 }
+
+
+
+
+
+
+
