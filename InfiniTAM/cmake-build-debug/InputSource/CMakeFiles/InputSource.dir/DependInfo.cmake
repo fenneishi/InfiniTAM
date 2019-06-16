0384117532b4cd1cffcf65a34ca7a8b0e464f5e6
龙 齐 <longqi@longdeMacBook-Pro.local>
2019-06-16 17:19:09 +0800

    delete

---
+++ InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/CompositeImageSourceEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/CompositeImageSourceEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/FFMPEGReader.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/FFMPEGReader.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/FFMPEGWriter.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/FFMPEGWriter.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/IMUSourceEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/IMUSourceEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/IdleImageSourceEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/IdleImageSourceEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/ImageSourceEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/ImageSourceEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/Kinect2Engine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/Kinect2Engine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/LibUVCEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/LibUVCEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/OpenNIEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/OpenNIEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/PicoFlexxEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/PicoFlexxEngine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/RealSense2Engine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/RealSense2Engine.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/InputSource/RealSenseEngine.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/InputSource/CMakeFiles/InputSource.dir/RealSenseEngine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILE_WITHOUT_CUDA"
+  "COMPILE_WITHOUT_Kinect2"
+  "COMPILE_WITHOUT_OpenNI"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/GLUT.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/GraphEdge.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/GraphEdge.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/GraphEdgeSE3.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/GraphEdgeSE3.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/LevenbergMarquardtMethod.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/LevenbergMarquardtMethod.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/MatrixWrapper.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/MatrixWrapper.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/PoseGraph.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/PoseGraph.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/QuaternionHelpers.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/QuaternionHelpers.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/SlamGraph.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/SlamGraph.cpp.o"
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/MiniSlamGraphLib/SlamGraphErrorFunction.cpp" "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/MiniSlamGraphLib/CMakeFiles/MiniSlamGraphLib.dir/SlamGraphErrorFunction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/longqi/Desktop/project/code/InfiniTAM/InfiniTAM/cmake-build-debug/ORUtils/CMakeFiles/ORUtils.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
